b4399b2a397f14b3b2913d778630ab7b30a6158f
gorkha <anme@odense.dk>
2013-11-18 13:43:13 +0100

    Moved ding-news super-heading to lead.

--- templates/node--ding-news.tpl.php
+++ templates/node--ding-news.tpl.php
@@ -87,19 +87,19 @@
  */
 ?>
 <?php  if ($page) : ?>
-<div class="super-heading">
-  <p>
-    <?php print render($content['field_ding_news_category']); ?>
-    <?php print render($content['field_ding_news_library']); ?>
-  </p>
-</div>
 <h2 class="heading"><?php print $title; ?></h2>
     <div class="grid-row">
     <div class="image-container">
       <?php print render($content['field_ding_news_title_image']); ?>
       <?php print render($content['field_ding_news_list_image']); ?>
     </div>
-    <div class="lead">
+      <div class="lead">
+          <div class="super-heading">
+            <p>
+              <?php print render($content['field_ding_news_category']); ?>
+              <?php print render($content['field_ding_news_library']); ?>
+            </p>
+          </div>
         <p>
       <?php print render($content['field_ding_news_lead'][0]); ?>
     </p>
