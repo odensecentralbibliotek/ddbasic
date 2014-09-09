c54d41c97df647712ce7516985c83875dcf46533
Tobias F. Petersen <tobsf@hotmail.com>
2014-09-09 09:16:20 +0200

    String Fix.

--- template.php
+++ template.php
@@ -216,7 +216,7 @@ function ddbasic_place2book_ticketsinfo($variables) {
       $output = '<div class="btn-warning btn-large">' . t('The event has already taken place') . '</div>';
       break;
     case 'closed-admission':
-      $output = '<div class="btn-warning btn-large">' . t('The event is closed for admission_s') . '</div>';
+      $output = '<div class="btn-warning btn-large">' . t('The event is closed for admission') . '</div>';
       break;
     case 'sale-not-started':
       $output = '<div class="btn-warning btn-large">' . t('Ticket sale has not yet started for this event') . '</div>';
