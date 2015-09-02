173e9fcee7c17e417ebe5c16c4a222f12011d1ad
Tobias F. Petersen <tobsf@hotmail.com>
2015-09-02 11:33:17 +0200

    Bad practice to have many h1's on same page.

--- templates/node--ding-library.tpl.php
+++ templates/node--ding-library.tpl.php
@@ -85,7 +85,7 @@
 ?>
 
 <div class="<?php print $classes; ?>">
-  <h1 class="page-title"><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h1>
+  <h2 class="page-title"><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
   <div class="content"<?php print $content_attributes; ?>>
     <?php
     if (isset($content['field_ding_library_list_image'])) {
